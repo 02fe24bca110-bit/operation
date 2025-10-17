0b96c6778a886db029923b0b8c34406b8ac34e1e
02fe24bca110-bit <02fe24bca110@gmail.com>
2025-10-17 09:21:02 +0530

    Update math.py

--- math.py
+++ math.py
@@ -2,6 +2,6 @@ try:
   a = float(input("Enter first number: "))
   b = float(input("Enter second number: "))
   print(f"Addition:{a + b}")
-    print(f"Substraction:{a - b}")
+  print(f"Substraction:{a - b}")
 except ValueError:
-print("Enter a valid no")
+  print("Enter a valid no")
